67b9481fa2244dae335c6ed8e322cc7196ec9913
Aleksandar Mechkaroski <alekoh@users.noreply.github.com>
2017-09-12 22:45:55 +0200

    Created company controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,12 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Demand;
+use App\Document;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index() {
+        $documents = Document::all()->orderBy('date', 'desc');
+        $demands = Demand::all()->where('status', 'active')->orderBy('date', 'asc');
+
+        $documents_approved = [];
+        $documents_declined = [];
+        $documents_review = [];
+
+        foreach ($documents as $document) {
+            if ($document->status == 1) {
+                array_push($documents_approved, $document);
+            } else if ($document->status == -1) {
+                array_push($documents_declined, $document);
+            } else if ($document->status == 0) {
+                array_push($documents_review, $document);
+            }
+        }
+
+        return view('company.home');
+    }
 }
